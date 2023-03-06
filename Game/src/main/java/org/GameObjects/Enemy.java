862736b8e676502f8f9f2aedc2ac74847d685de8
apa109 <apa109@sfu.ca>
2023-03-06 14:14:13 -0800

    Created Enemy.java (no implementation yet)

---
+++ Game/src/main/java/org/GameObjects/Enemy.java
@@ -0,0 +1,15 @@
+package org.GameObjects;
+import org.javatuples.Pair;
+
+public class Enemy extends MovableObject {
+    // ArrayList of pairs where each pair represents a tile on the 2D tilemap
+    ArrayList<Pair<Integer,Integer>> shortestPathToMainCharacter;
+
+    public Enemy() {
+        // initialize shortestPathToMainCharacter
+    }
+
+    public void updatePosition() {
+        // implement me
+    }
+}
