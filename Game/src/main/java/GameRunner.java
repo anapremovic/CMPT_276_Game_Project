7517ebd0e0aae4825744466e524c443f38b2fba4
apa109 <apa109@sfu.ca>
2023-03-07 01:29:05 -0800

    Created basic screen functionality

--- Game/src/main/java/GameRunner.java
+++ Game/src/main/java/GameRunner.java
@@ -1,5 +1,7 @@
 public class GameRunner {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        new Screen("CMPT 276 Game", 800, 600);
     }
 }
--- Game/src/main/java/Screen.java
+++ Game/src/main/java/Screen.java
@@ -1,2 +1,20 @@
-public class Screen {
+import java.awt.Canvas;
+import javax.swing.JFrame;
+
+public class Screen extends Canvas implements Runnable {
+    JFrame frame;
+
+    public Screen(String title, int width, int height) {
+        frame = new JFrame(title);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(width, height);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.add(this);
+    }
+
+    public void run() {
+
+    }
 }
