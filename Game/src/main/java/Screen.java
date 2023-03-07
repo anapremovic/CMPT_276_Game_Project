6dbc89e0e70056eb221b8be1ff1d9a4907a861ef
apa109 <apa109@sfu.ca>
2023-03-07 02:11:24 -0800

    update game loop logic

--- Game/src/main/java/Screen.java
+++ Game/src/main/java/Screen.java
@@ -54,10 +54,37 @@ public class Screen extends Canvas implements Runnable {
     }
 
     public void run() {
-        while(running) {
-            tick();
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+
+        while (running)
+        {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1)
+            {
+                tick();
+                updates++;
+                delta--;
+            }
             Render();
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000)
+            {
+                timer += 1000;
+                System.out.println("FPS: " + frames + " TICKS: " + updates);
+                frames = 0;
+                updates = 0;
+            }
         }
+
+        stop();
     }
 
     // update screen
