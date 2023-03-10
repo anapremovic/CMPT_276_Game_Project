33ab4f415688590d2da5d551b017c34f8dbcee50
apa109 <apa109@sfu.ca>
2023-03-10 14:34:08 -0800

    Started restructing classes to follow our new design

--- Game/src/main/java/org/Display/GameRunner.java
+++ Game/src/main/java/org/Display/GameRunner.java
@@ -2,13 +2,25 @@ package org.Display;
 
 import org.Logic.MenuLogic;
 
+import javax.swing.*;
+
 public class GameRunner {
     Screen screen;
     MenuLogic menuLogic;
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        Screen screen = new Screen("CMPT 276 Game", 800, 600);
-        screen.start();
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // closes the program when you click X
+        window.setResizable(false); // do not allow resizing
+        window.setTitle("Group 4 Game");
+
+        Screen screen = new Screen();
+        window.add(screen);
+
+        window.pack(); // size window to fit size of screen
+
+        window.setLocationRelativeTo(null); // window will be at centre of screen
+        window.setVisible(true);
     }
 }
--- Game/src/main/java/org/Display/Screen.java
+++ Game/src/main/java/org/Display/Screen.java
@@ -1,139 +1,37 @@
 package org.Display;
 
-import org.GameObjects.*;
-
-import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferStrategy;
+import javax.swing.*;
 
-public class Screen extends Canvas implements Runnable {
-    JFrame frame;
-    private Thread thread;
-    private boolean running;
-    int width;
-    int height;
+public class Screen extends JPanel implements Runnable {
+    // settings
+    final int baseTileSize = 16;
+    final int scale = 3;
 
-    // temporarily in Screen
-    private MainCharacter player = new MainCharacter(100, 100, 64, 64);
+    final int tileSize = baseTileSize * scale; // 48x48 tile size
+    final int screenColumns = 18;
+    final int screenRows = 12;
+    final int screenWidth = tileSize * screenColumns;
+    final int screenHeight = tileSize * screenRows;
 
-    public Screen(String title, int w, int h) {
-        running = false;
-        frame = new JFrame(title);
+    // thread
+    Thread gameThread;
 
-        width = w;
-        height = h;
-
-        // screen properties
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // closes the program when you click X
-        frame.setSize(width, height); // set size of the screen
-        frame.setResizable(false); // do not allow resizing
-        frame.setVisible(true); // set the screen to visible
-        frame.add(this);
+    public Screen() {
+        // set screen size
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.gray);
+        this.setDoubleBuffered(true); // improves rendering performance
     }
 
-    public int getWidth() {
-        return width;
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start(); // call run method
     }
-
-    public int getHeight() {
-        return height;
-    }
-
-    // start game logic
-    public void start() {
-        running = true;
-
-        // create our new thread
-        thread = new Thread(this);
-        thread.start();
-    }
-
-    // stop game logic
-    public void stop() {
-        running = false;
-
-        // stop our thread
-        try {
-            thread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
+    
+    // GAME LOOP
+    @Override
     public void run() {
-        long lastTime = System.nanoTime();
-        double amountOfTicks = 60.0;
-        double ns = 1000000000 / amountOfTicks;
-        double delta = 0;
-        long timer = System.currentTimeMillis();
-        int updates = 0;
-        int frames = 0;
 
-        while (running)
-        {
-            long now = System.nanoTime();
-            delta += (now - lastTime) / ns;
-            lastTime = now;
-            while (delta >= 1)
-            {
-                tick();
-                updates++;
-                delta--;
-            }
-            Render();
-            frames++;
-            if (System.currentTimeMillis() - timer > 1000)
-            {
-                timer += 1000;
-                System.out.println("FPS: " + frames + " TICKS: " + updates);
-                frames = 0;
-                updates = 0;
-            }
-        }
-
-        stop();
-    }
-
-    // update screen
-    private void tick() {
-        player.tick();
-    }
-
-    // paint onto window
-    private void Render() {
-        BufferStrategy bs = this.getBufferStrategy(); // makes transitions smoother
-
-        // if haven't created BufferStrategy yet, create it
-        if(bs == null) {
-            this.createBufferStrategy(2);
-            bs = this.getBufferStrategy();
-        }
-
-        Graphics g = bs.getDrawGraphics(); // create graphics
-
-        // test if Render works
-        g.setColor(Color.blue);
-        g.fillRect(0, 0, width, height);
-
-        player.Render(g);
-
-        bs.show();
-        g.dispose(); // clear the screen
-    }
-
-    public void MainMenu() {
-        // implement me
-    }
-
-    public void WiningScreen() {
-        // implement me
-    }
-
-    public void GameScreen() {
-        // implement me
-    }
-
-    public void GameOverScreen() {
-        // implement me
     }
 }
