9cdb1449d169025d255de4e01925f9a17ddb9ac3
apa109 <apa109@sfu.ca>
2023-03-12 14:49:17 -0700

    Created class for displaying regular and bonus rewards to screen

---
+++ Game/src/main/java/org/Display/ImmovableObjectDisplay.java
@@ -0,0 +1,9 @@
+package org.Display;
+
+public class ImmovableObjectDisplay {
+    private Screen screen;
+
+    public ImmovableObjectDisplay(Screen screen) {
+        this.screen = screen;
+    }
+}
