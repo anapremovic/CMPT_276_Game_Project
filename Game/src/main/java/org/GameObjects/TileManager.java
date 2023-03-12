b10c3dc765d5aaa1278066fae9535f51f359cca9
apa109 <apa109@sfu.ca>
2023-03-11 21:13:25 -0800

    added comments to TileManager

--- Game/src/main/java/org/GameObjects/TileManager.java
+++ Game/src/main/java/org/GameObjects/TileManager.java
@@ -12,19 +12,19 @@ import javax.imageio.ImageIO;
 
 public class TileManager {
     private Screen screen;
-    private Tile[] tiles; // all the different types of tiles
-    int board[][]; // 2D array of ints, where each int represents the tile type in the array tiles for that position
+    private Tile[] tileTypes; // all the different types of tiles
+    int board[][]; // 2D array of ints, where each int represents the tile type corresponding to tiles[] for that position
 
     public TileManager(Screen screen) {
-
         this.screen = screen;
-        tiles = new Tile[4]; // we have 4 tile types in our game
+        tileTypes = new Tile[4]; // we have 4 tile types in our game
         board = new int[screen.getNumColumns()][screen.getNumRows()];
 
         getTileImages();
         loadBoardData("/maps/map01.txt");
     }
 
+    // load board data from given txt file into board[][]
     public void loadBoardData(String boardFilePath) {
         try {
             InputStream is = getClass().getResourceAsStream(boardFilePath); // load map file
@@ -37,7 +37,7 @@ public class TileManager {
                 for(int col = 0; col < screen.getNumColumns(); col++) {
                     int curTileNum = Integer.parseInt(curLineNumbers[col]); // current tile num at this row and col
 
-                    board[col][row] = curTileNum; // load into board 2D array
+                    board[col][row] = curTileNum; // load board data
                 }
             }
 
@@ -47,36 +47,45 @@ public class TileManager {
         }
     }
 
+    // load all possible tile images into tileTypes[]
     public void getTileImages() {
 
         try {
-            tiles[0] = new Tile();
-            tiles[0].image = ImageIO.read(getClass().getResourceAsStream("/cave_background.png"));
+            // ground tile
+            tileTypes[0] = new Tile();
+            tileTypes[0].image = ImageIO.read(getClass().getResourceAsStream("/cave_background.png"));
 
-            tiles[1] = new Tile();
-            tiles[1].image = ImageIO.read(getClass().getResourceAsStream("/outside_barrier_tile.png"));
+            // outside barrier
+            tileTypes[1] = new Tile();
+            tileTypes[1].image = ImageIO.read(getClass().getResourceAsStream("/outside_barrier_tile.png"));
 
-            tiles[2] = new Tile();
-            tiles[2].image = ImageIO.read(getClass().getResourceAsStream("/rock_tile.png"));
+            // inside barrier
+            tileTypes[2] = new Tile();
+            tileTypes[2].image = ImageIO.read(getClass().getResourceAsStream("/rock_tile.png"));
 
-            tiles[3] = new Tile();
-            tiles[3].image = ImageIO.read(getClass().getResourceAsStream("/lava.png"));
+            // punishment - lava
+            tileTypes[3] = new Tile();
+            tileTypes[3].image = ImageIO.read(getClass().getResourceAsStream("/lava.png"));
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    // draw tiles onto screen
     public void draw(Graphics2D g) {
         for(int row = 0; row < screen.getNumRows(); row++) {
-
             for(int col = 0; col < screen.getNumColumns(); col++) {
+                // current x and y position of this tile in pixels
                 int curXPos = col * screen.getTileSize();
                 int curYPos = row * screen.getTileSize();
 
+                // current tile type number - corresponding to tileTypes
                 int curTileNum = board[col][row];
 
-                g.drawImage(tiles[curTileNum].image, curXPos, curYPos, screen.getTileSize(), screen.getTileSize(), null);
+                // draw corresponding tile onto screen
+                g.drawImage(tileTypes[curTileNum].image, curXPos, curYPos,
+                        screen.getTileSize(), screen.getTileSize(), null);
             }
         }
     }
