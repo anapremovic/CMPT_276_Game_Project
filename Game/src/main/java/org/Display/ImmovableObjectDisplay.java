091f2162fee8c08dc27b4cea683494a1029e1cb1
apa109 <apa109@sfu.ca>
2023-03-12 16:48:09 -0700

    Implemented functionality to display 10 random-position carrots

--- Game/src/main/java/org/Display/ImmovableObjectDisplay.java
+++ Game/src/main/java/org/Display/ImmovableObjectDisplay.java
@@ -2,18 +2,47 @@ package org.Display;
 
 import org.GameObjects.*;
 
+import java.util.Random;
+
 public class ImmovableObjectDisplay {
     private Screen screen;
+    private TileManager tileM;
 
-    public ImmovableObjectDisplay(Screen screen) {
+    public ImmovableObjectDisplay(Screen screen, TileManager tileM) {
         this.screen = screen;
+        this.tileM = tileM;
     }
 
     public void displayObjects() {
-        RegularReward carrot1 = new RegularReward(3 * screen.getTileSize(), 2 * screen.getTileSize());
-        screen.setReward(0, carrot1);
 
-        RegularReward carrot2 = new RegularReward(10 * screen.getTileSize(), 4 * screen.getTileSize());
-        screen.setReward(1, carrot2);
+
+        Random rand = new Random();
+
+        int[][] board = tileM.getBoard();
+        int numRewards = screen.getRewards().length;
+
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // generate 10 carrots
+        for(int i = 0; i < numRewards - 1; i++) {
+            // generate random position for the current reward
+            int randomXPos = rand.nextInt(screen.getNumColumns());
+            int randomYPos = rand.nextInt(screen.getNumRows());
+
+            // make sure the position is on an empty tile
+            while(board[randomXPos][randomYPos] != 0) {
+                randomXPos = rand.nextInt(screen.getNumColumns());
+                randomYPos = rand.nextInt(screen.getNumRows());
+            }
+
+            RegularReward cur = new RegularReward((randomXPos) * screen.getTileSize(),
+                    (randomYPos) * screen.getTileSize());
+            screen.setReward(i, cur);
+        }
     }
 }
