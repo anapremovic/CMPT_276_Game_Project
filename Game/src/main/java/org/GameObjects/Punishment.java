16348ad3b6457d280cfb61fd09dfdca80f248481
apa109 <apa109@sfu.ca>
2023-03-13 18:57:46 -0700

    Implemented Punishment class

--- Game/src/main/java/org/GameObjects/Punishment.java
+++ Game/src/main/java/org/GameObjects/Punishment.java
@@ -1,12 +1,29 @@
 package org.GameObjects;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+
 public class Punishment extends ImmovableObject {
     private int damage;
 
-    public Punishment(int damage) {
-        this.damage = damage;
-    }
-    public int getDamage() {
-        return damage;
+    public Punishment(int xPos, int yPos) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.damage = 3;
+
+        //this.points = points;
+        this.name = "Lava";
+
+        this.collidableArea = new Rectangle(0, 0, 48, 48);
+        this.collidableAreaDefaultX = collidableArea.x;
+        this.collidableAreaDefaultY = collidableArea.y;
+
+        // load lava image
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("/lava.png"));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
