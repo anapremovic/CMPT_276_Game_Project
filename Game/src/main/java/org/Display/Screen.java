74d7472a6ae00dce71827400bc23f913b1f0cb24
Your Name <zya89@sfu.ca>
2023-03-14 02:22:49 -0700

    add enemy to the map

--- Game/src/main/java/org/Display/Screen.java
+++ Game/src/main/java/org/Display/Screen.java
@@ -6,6 +6,7 @@ import org.Logic.*;
 
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
@@ -31,7 +32,7 @@ public class Screen extends JPanel implements Runnable {
 
 
     // game objects
-
+    ArrayList<Enemy> enemies = new ArrayList<>();
     // display up to 10 carrots, 1 mystical ocean fruit, and 6 lava at one time
     private ImmovableObject objects[] = new ImmovableObject[17];
     private ImmovableObjectDisplay objDisplayer = new ImmovableObjectDisplay(this, gameTiles);
@@ -66,7 +67,7 @@ public class Screen extends JPanel implements Runnable {
         // display menu at start of program
         menuLogic = new MenuLogic(this);
         menuLogic.displayMenu();
-
+        enemies.add(new Enemy(this, collisionDetector, gameTiles));
         // initialize elapsedTime to 0
         elapsedTime = 0;
     }
@@ -80,16 +81,17 @@ public class Screen extends JPanel implements Runnable {
 
     public void startGameThread() {
         gameThread = new Thread(this);
-            gameThread.start(); // call run() method
+        gameThread.start(); // call run() method
     }
+
     public void endGameThread() {
         gameThread.interrupt();
     }
-    
+
     // GAME LOOP
     @Override
     public void run() {
-        double drawInterval = 1000000000/FPS; // 1 second/60
+        double drawInterval = 1000000000 / FPS; // 1 second/60
         //double nextDrawTime = System.nanoTime() * drawInterval; // next time to draw the screen
         double delta = 0;
         long lastTime = System.nanoTime();
@@ -97,22 +99,22 @@ public class Screen extends JPanel implements Runnable {
         long timer = 0;
         int drawCount = 0;
 
-        while(gameThread != null) {
+        while (gameThread != null) {
             currentTime = System.nanoTime();
 
             delta += (currentTime - lastTime) / drawInterval;
             timer += (currentTime - lastTime);
             lastTime = currentTime;
 
-            if(delta >= 1) {
+            if (delta >= 1) {
                 // score goes negative => game over
-                if(this.score < 0) {
+                if (this.score < 0) {
                     endGameThread();
                     gameOverMenu.displayGameOverMenu();
                 }
 
                 // score reaches 10 => exit unlocks
-                if(this.score >= 10) {
+                if (this.score >= 10) {
                     gameTiles.setMap("/maps/map02.txt");
                 }
 
@@ -124,7 +126,7 @@ public class Screen extends JPanel implements Runnable {
                 drawCount++;
             }
 
-            if(timer >= 1000000000) {
+            if (timer >= 1000000000) {
                 System.out.println("FPS: " + drawCount);
                 drawCount = 0;
                 timer = 0;
@@ -134,7 +136,12 @@ public class Screen extends JPanel implements Runnable {
 
     public void update() {
         player.update();
+        for (Enemy e:enemies)
+        {
+            e.update(player,tileSize);
+        }
     }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
@@ -144,15 +151,18 @@ public class Screen extends JPanel implements Runnable {
         gameTiles.draw(g2); // important to draw tiles before game objects
 
         // IMMOVABLE OBJECTS
-        for(ImmovableObject cur : objects) {
-            if(cur != null) {
+        for (ImmovableObject cur : objects) {
+            if (cur != null) {
                 cur.draw(g2, this);
             }
         }
 
         // MAIN CHARACTER
         player.draw(g2);
-
+        for (Enemy e:enemies)
+        {
+            e.draw(g2);
+        }
         // TIMER
         g2.setFont(timerFont);
         g2.setColor(new Color(255, 255, 255, 150));
@@ -175,21 +185,40 @@ public class Screen extends JPanel implements Runnable {
     public int getTileSize() {
         return tileSize;
     }
-    public int getNumColumns() { return screenColumns; }
-    public int getNumRows() { return screenRows; }
-    public int getScreenWidth() { return screenWidth; }
-    public int getScreenHeight() { return screenHeight; }
 
-    public ImmovableObject[] getObjects() { return objects; }
-    public WinningMenu getWinningMenu() { return winningMenu; }
+    public int getNumColumns() {
+        return screenColumns;
+    }
+
+    public int getNumRows() {
+        return screenRows;
+    }
+
+    public int getScreenWidth() {
+        return screenWidth;
+    }
+
+    public int getScreenHeight() {
+        return screenHeight;
+    }
+
+    public ImmovableObject[] getObjects() {
+        return objects;
+    }
+
+    public WinningMenu getWinningMenu() {
+        return winningMenu;
+    }
 
     // setters
     public void setObject(int index, ImmovableObject newObject) {
         objects[index] = newObject;
     }
+
     public void setObjectCollidableAreaX(int index, int x) {
         objects[index].setCollidableAreaX(x);
     }
+
     public void setObjectCollidableAreaY(int index, int y) {
         objects[index].setCollidableAreaY(y);
     }
