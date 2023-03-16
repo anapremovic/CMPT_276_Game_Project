a0f32e81bc213a3e2d68a75d44b74b2a52f73966
apa109 <apa109@sfu.ca>
2023-03-16 01:38:10 -0700

    Added javadoc to Punishment.java

--- Game/src/main/java/org/GameObjects/Punishment.java
+++ Game/src/main/java/org/GameObjects/Punishment.java
@@ -4,12 +4,23 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.IOException;
 
+/**
+ * Entity encoding information on game punishments: lava tiles which cause the player to lose points and return to the
+ * starting position when touched.
+ */
 public class Punishment extends ImmovableObject {
+
+    /**
+     * Initialize this punishment to a given x and y position, as well as initializing all its variables and loading
+     * its image file.
+     *
+     * @param xPos      x position of punishment on screen
+     * @param yPos      y position of punishment on screen
+     */
     public Punishment(int xPos, int yPos) {
         this.xPos = xPos;
         this.yPos = yPos;
 
-        //this.points = points;
         this.name = "Lava";
 
         this.collidableArea = new Rectangle(0, 0, 48, 48);
