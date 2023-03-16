2ad71a600a19b2970deeabca49266606508670a9
apa109 <apa109@sfu.ca>
2023-03-16 01:39:59 -0700

    Added javadoc to RegularReward.java

--- Game/src/main/java/org/GameObjects/RegularReward.java
+++ Game/src/main/java/org/GameObjects/RegularReward.java
@@ -4,7 +4,19 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.IOException;
 
+/**
+ * Entity encoding information on game punishments: carrots which grant the player points. The player must collect
+ * all 10 carrots in order to escape the cave and win the game.
+ */
 public class RegularReward extends Reward {
+
+    /**
+     * Initialize this reward to a given x and y position, as well as initializing all its variables and loading
+     * its image file.
+     *
+     * @param xPos      x position of punishment on screen
+     * @param yPos      y position of punishment on screen
+     */
     public RegularReward(int xPos, int yPos) {
         this.xPos = xPos;
         this.yPos = yPos;
@@ -23,7 +35,7 @@ public class RegularReward extends Reward {
             e.printStackTrace();
         }
     }
-    public int getPoints() {
-        return points;
-    }
+
+    // GETTERS
+    public int getPoints() { return points; }
 }
