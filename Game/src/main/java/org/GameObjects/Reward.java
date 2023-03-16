84d4082b4c582fa5d2fcd028050357e82255c553
apa109 <apa109@sfu.ca>
2023-03-16 01:41:25 -0700

    Added javadoc to Reward.java

--- Game/src/main/java/org/GameObjects/Reward.java
+++ Game/src/main/java/org/GameObjects/Reward.java
@@ -1,7 +1,17 @@
 package org.GameObjects;
 
+/**
+ * Abstract class representing any immovable object on the screen that is a reward. In other words, that grants points
+ * to the player when collected.
+ */
 public abstract class Reward extends ImmovableObject {
+
+    /**
+     * Number of points that this reward grants.
+     */
     protected int points;
 
+    // GETTERS
+    
     public abstract int getPoints();
 }
