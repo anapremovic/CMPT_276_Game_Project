5dc66eaf7ea32e67373998f061395687450c59ac
apa109 <apa109@sfu.ca>
2023-04-04 20:20:45 -0700

    Created TileManagerTest class

---
+++ Game/src/test/java/org/GameObjects/TileManagerTest.java
@@ -0,0 +1,32 @@
+package org.GameObjects;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TileManagerTest {
+
+    @Test
+    void loadBoardData() {
+    }
+
+    @Test
+    void getTileImages() {
+    }
+
+    @Test
+    void draw() {
+    }
+
+    @Test
+    void getBoard() {
+    }
+
+    @Test
+    void getTileTypes() {
+    }
+
+    @Test
+    void setMap() {
+    }
+}
