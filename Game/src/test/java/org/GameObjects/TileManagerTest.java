5bd08a956d0e8d3d30cb6594aceec6586c01a176
apa109 <apa109@sfu.ca>
2023-04-04 20:48:38 -0700

    Implemented tileManager tests

--- Game/src/test/java/org/GameObjects/TileManagerTest.java
+++ Game/src/test/java/org/GameObjects/TileManagerTest.java
@@ -1,32 +1,57 @@
 package org.GameObjects;
 
+import org.Display.Screen;
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TileManagerTest {
+    Screen screen = new Screen();
 
     @Test
     void loadBoardData() {
+        TileManager tiles = new TileManager(screen);
+
+        tiles.loadBoardData("/map_mock.txt");
+
+        assertNotNull(tiles.getBoard());
+
+        int[][] board = tiles.getBoard();
+
+        // check that map loaded correctly
+        assertEquals(0, board[0][0]);
+        assertEquals(1, board[1][0]);
+        assertEquals(2, board[0][1]);
+        assertEquals(3, board[1][1]);
     }
 
     @Test
     void getTileImages() {
-    }
+        TileManager tiles = new TileManager(screen);
 
-    @Test
-    void draw() {
+        Tile[] tileTypes = tiles.getTileTypes();
+
+        tiles.getTileImages();
+
+        // assert we have all 4 tile images
+        for(int i = 0; i < 4; i++) {
+            assertNotNull(tileTypes[i].image);
+        }
     }
 
     @Test
     void getBoard() {
+        TileManager tiles = new TileManager(screen);
+        int[][] board = tiles.getBoard();
+        assertNotNull(board);
     }
 
     @Test
     void getTileTypes() {
-    }
-
-    @Test
-    void setMap() {
+        TileManager tiles = new TileManager(screen);
+        Tile[] tileTypes = tiles.getTileTypes();
+        assertNotNull(tileTypes);
     }
 }
